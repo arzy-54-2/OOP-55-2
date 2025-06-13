883b0f3028112228f6c3068c69a69d544aff527a
arzy-54-2 <qigeeks@gmail.com>
2025-06-13 19:09:04 +0600

    update: lesson 2 .py

--- lessons/lesson2.py
+++ lessons/lesson2.py
@@ -5,7 +5,7 @@
 # Родительский класс/ Супер класс
 class Hero:
 
-    def __init__(self, name, lvl, hp):
+    def __init__(self, name, hp, lvl=None):
         self.name = name
         self.lvl = lvl
         self.hp = hp
@@ -17,14 +17,18 @@ class Hero:
 # Дочерний класс/Класс наследник
 class WarriorHero(Hero):
 
-    def __init__(self, name, lvl, hp, st):
-        super().__init__(name, lvl, hp)
+    def __init__(self, name, hp, st):
+        super().__init__(name, hp)
         self.st = st
 
     def action(self):
         print(f"WarriorHero method!!!")
 
-hero_1 = WarriorHero("Hero 1", 11, 100, 1000)
+hero_1 = WarriorHero("Hero 1", 11, 100)
 
 hero_1.action()
+
+# CamelCase
+# snake_case
+
 
